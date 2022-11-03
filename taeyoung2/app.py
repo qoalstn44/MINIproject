b45c83d05bf5b352d66f77d4c81959d75f3ccf43
tyoung <taeyoung917@gmail.com>
2022-11-03 14:55:24 +0900

    팀원 DB, like 추가

--- taeyoung2/app.py
+++ taeyoung2/app.py
@@ -11,26 +11,40 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+# 팀원추가
+@app.route("/team", methods=["POST"])
+def team_post():
+    teams_receive = request.form['teams_give']
+    intro_receive = request.form['intro_give']
+    photo_recevie = request.form['photo_give']
+    team_list = list(db.oneBillion.find({}, {'_id': False}))
+    count = len(team_list) + 1
 
-@app.route('/like', methods=['GET'])
-def test_get():
-    like_list = list(db.mini.find({}, {'_id': False}))
-    return jsonify({'likes': like_list})
+    doc = {
+        'like':0,
+        'teams':teams_receive,
+        'intro': intro_receive,
+        'num':count,
+        'photo':photo_recevie
+    }
+    db.oneBillion.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
 
-
-
-@app.route('/like', methods=['POST'])
-def like_star():
-    name_receive = request.form['name_give']
-
-    target_star = db.mystar.find_one({'name': name_receive})
+# 좋아요수 post
+@app.route("/api/like", methods=["POST"])
+def like_post():
+    num_receive = request.form['num_give']
+    target_star = db.oneBillion.find_one({'num': int(num_receive)})
     current_like = target_star['like']
-
     new_like = current_like + 1
+    db.oneBillion.update_one({'num': int(num_receive)}, {'$set': {'like': new_like}})
+    return jsonify({'msg': 'like 연결되었습니다!'})
 
-    db.mini.update_one({'name': name_receive}, {'$set': {'like': new_like}})
-
-    return jsonify({'msg': '좋아요 완료!'})
+# 추가된 팀원 get
+@app.route("/team", methods=["GET"])
+def team_get():
+    teams_list = list(db.oneBillion.find({}, {'_id': False}))
+    return jsonify({'teams': teams_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
